d2266b52d5b1dbd49531517f6745a1a95de4a8f3
Shane <shane@swalker.dev>
2023-12-27 08:13:51 -0600

    implmented home sign search

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { Form, useSearchParams, useSubmit } from "@remix-run/react";
 import { Input } from "~/components/ui/input";
 import { VideoCard } from "~/components/video-card";
 import { prisma } from "~/db.server";
+import { useDebounce } from "~/hooks/useDebounce";
 import { getUserId } from "~/services/auth.server";
 import { superjson, useSuperLoaderData } from "~/utils/data";
 
@@ -14,6 +16,19 @@ export const meta: MetaFunction = () => {
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const maybeUser = await getUserId(request);
+  const searchTerm = new URL(request.url).searchParams.get("search");
+  if (searchTerm) {
+    const videos = await prisma.video.findMany({
+      where: {
+        status: "ACTIVE",
+        sign: { term: { contains: searchTerm } }
+      },
+      include: { sign: true, votes: true },
+      take: 10,
+      orderBy: { uploadDate: "desc" }
+    });
+    return superjson({ videos, userId: maybeUser });
+  }
   const videos = await prisma.video.findMany({
     where: { status: "ACTIVE" },
     include: { sign: true, votes: true },
@@ -24,13 +39,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Index() {
+  const [searchParams] = useSearchParams();
+  const submit = useSubmit();
   const { videos, userId } = useSuperLoaderData<typeof loader>();
+  const handleFormChange = useDebounce((form: HTMLFormElement) => {
+    submit(form, { replace: true });
+  }, 500);
   return (
     <div className="flex flex-col gap-4 pt-4">
-      <Input
-        className="h-16 text-2xl md:h-20 md:text-2xl"
-        placeholder="Find a Sign..."
-      />
+      <Form method="GET" onChange={(e) => handleFormChange(e.currentTarget)}>
+        <Input
+          name="search"
+          defaultValue={searchParams.get("search") || ""}
+          className="h-16 text-2xl md:h-20 md:text-2xl"
+          placeholder="Find a Sign..."
+        />
+      </Form>
       {videos.map((video) => (
         <div
           key={video.id}
