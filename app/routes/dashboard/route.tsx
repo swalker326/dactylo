05625cadf8fcedf32d73504ec28d4d36faf3a197
Shane <shane@swalker.dev>
2023-12-27 07:40:13 -0600

    combined create and upload

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -1,6 +1,6 @@
 import { MetaFunction } from "@remix-run/node";
 import { NavLink, Outlet } from "@remix-run/react";
-import { CameraIcon, Home, Settings, Upload } from "lucide-react";
+import { CameraIcon, Home, Settings } from "lucide-react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -51,18 +51,6 @@ export default function DashboardRoute() {
           to="settings"
         >
           <Settings size={28} />
-        </NavLink>
-        <NavLink
-          unstable_viewTransition
-          end
-          className={({ isActive }) => {
-            return `px-2 py-2 flex justify-center items-center flex-1  ${
-              isActive ? "bg-gray-200 text-blue-400" : ""
-            }`;
-          }}
-          to="upload"
-        >
-          <Upload size={28} />
         </NavLink>
       </div>
       <div className="pt-3 pb-8">
