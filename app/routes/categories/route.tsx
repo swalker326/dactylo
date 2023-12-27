3a6cdda36814f6309cc6e518141abd1ee48ab562
Shane <shane@swalker.dev>
2023-12-26 22:46:49 -0600

    added stub routes

---
+++ app/routes/categories/route.tsx
@@ -0,0 +1,7 @@
+export default function CategoriesRoute() {
+  return (
+    <div>
+      <h3 className="font-3xl">Categories</h3>
+    </div>
+  );
+}
---
+++ app/routes/trending/route.tsx
@@ -0,0 +1,7 @@
+export default function TrendingRoute() {
+  return (
+    <div>
+      <h3 className="font-3xl">Trending</h3>
+    </div>
+  );
+}
