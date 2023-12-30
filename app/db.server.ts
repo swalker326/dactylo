2580047d3823bddfee1e180612f3919423dfcd3c
Shane <shane@swalker.dev>
2023-12-29 21:00:24 -0600

    removed boostedPrisma

--- app/db.server.ts
+++ app/db.server.ts
@@ -5,7 +5,5 @@ import { singleton } from "~/singleton.server";
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 const prisma = singleton("prisma", () => new PrismaClient());
 prisma.$connect();
-const boostedPrisma = new PrismaClient();
-await boostedPrisma.$queryRaw`SET @@boost_cached_queries = true`;
 
-export { prisma, boostedPrisma };
+export { prisma };
