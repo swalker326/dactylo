8ab1d13bfebd4ade0d005f42fbc44b4d5edc390f
Shane <shane@swalker.dev>
2024-01-24 19:38:02 -0600

    improved login form

--- apps/remix/app/routes/auth.login/route.tsx
+++ apps/remix/app/routes/auth.login/route.tsx
@@ -7,6 +7,7 @@ import {
 } from "@remix-run/node";
 import {
 	Form,
+	Link,
 	useActionData,
 	useNavigation,
 	useSearchParams,
@@ -20,6 +21,8 @@ import { PasswordSchema } from "~/utils/user-validation";
 import { handleNewSession } from "./login";
 import { ProviderConnectionForm, providerNames } from "~/utils/connections";
 import { StatusButton } from "~/components/ui/status-button";
+import { Button } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
 
 const LoginFormSchema = z.object({
 	email: z.string().email(),
@@ -47,54 +50,86 @@ export default function LoginRoute() {
 	const navigation = useNavigation();
 
 	return (
-		<div className="mt-5 flex justify-center">
-			<div className=" w-full md:w-1/2 md:p-4 rounded-md">
-				{actionData?.submission.error.login && (
-					<div className="p-3 bg-red-200 text-red-600 rounded-md">
-						{actionData.submission.error.login}
-					</div>
-				)}
-				<Form method="POST" className="max-w-3xl" {...form.props}>
-					<AuthenticityTokenInput />
-					<div className="flex flex-col gap-y-2 w-full">
-						<Input placeholder="email" type="email" name="email" required />
-						{fields.email.error && (
-							<p className="text-red-500">{fields.email.error}</p>
-						)}
-						<Input
-							placeholder="password"
-							type="password"
-							name="password"
-							autoComplete="current-password"
-							required
-						/>
-						{fields.email.error && (
-							<p className="text-red-500">{fields.password.error}</p>
-						)}
-						<StatusButton
-							type="submit"
-							status={
-								navigation.formAction === "/auth/login" ? "loading" : "idle"
-							}
-							className="flex gap-1"
-							message="Logging in..."
-						>
-							<p>Sign In</p>
-						</StatusButton>
-					</div>
-				</Form>
-				<div className="flex flex-col gap-2">
-					<h3 className="py-2 text-xl">Login Methods</h3>
-					<div className="p-4 border border-gray-400 rounded-md flex gap-2 flex-wrap justify-center">
-						{providerNames.map((providerName) => (
-							<ProviderConnectionForm
-								key={providerName}
-								type="Login"
-								providerName={providerName}
-								redirectTo={redirectTo}
+		<div className="flex justify-center p-1.5 flex-col w-full">
+			<div>
+				<div className="md:p-4">
+					{actionData?.submission.error.login && (
+						<div className="p-3 bg-red-200 text-red-600 rounded-md">
+							{actionData.submission.error.login}
+						</div>
+					)}
+					<Form method="POST" {...form.props}>
+						<AuthenticityTokenInput />
+						<div className="flex flex-col gap-y-2 w-full pb-2">
+							<Input placeholder="Email" type="email" name="email" required />
+							{fields.email.error && (
+								<p className="text-red-500">{fields.email.error}</p>
+							)}
+							<Input
+								placeholder="Password"
+								type="password"
+								name="password"
+								autoComplete="current-password"
+								required
 							/>
-						))}
+							<div className="flex justify-end">
+								<Link
+									to="/auth/onboarding"
+									className="text-blue-500 opacity-60 float-right"
+								>
+									forgot password?
+								</Link>
+							</div>
+							{fields.email.error && (
+								<p className="text-red-500">{fields.password.error}</p>
+							)}
+							<div className="flex justify-between items-center">
+								<StatusButton
+									type="submit"
+									status={
+										navigation.formAction === "/auth/login" ? "loading" : "idle"
+									}
+									className="flex gap-1"
+									message="Logging in..."
+								>
+									<p>Sign In</p>
+								</StatusButton>
+							</div>
+						</div>
+					</Form>
+					<div className="relative py-2">
+						<div className="text-body-xs uppercase absolute left-1/2 bg-gray-100 px-3 text-gray-600 opacity-55 -translate-x-1/2 top-1/2 -translate-y-1/2">
+							<p>or login with</p>
+						</div>
+						<Separator className="my-4" />
 					</div>
+					<div className="flex flex-col gap-2 p-4 bg-white rounded-lg">
+						<div className="p-4 rounded-md flex gap-2 flex-wrap justify-center">
+							{providerNames.map((providerName) => (
+								<ProviderConnectionForm
+									key={providerName}
+									type="Login"
+									providerName={providerName}
+									redirectTo={redirectTo}
+								/>
+							))}
+						</div>
+					</div>
+					<div className="relative py-2">
+						<div className="text-nowrap text-body-xs uppercase absolute left-1/2 bg-gray-100 px-3 top-1/2 -translate-y-1/2 text-gray-600 -translate-x-1/2">
+							<p className="opacity-55">or create an account</p>
+						</div>
+						<Separator className="my-4" />
+					</div>
+
+					<Button variant="outline" asChild>
+						<Link
+							to="/auth/signup"
+							className="w-full px-2 py-1.5 border border-gray-500 rounded-lg"
+						>
+							Create an Account
+						</Link>
+					</Button>
 				</div>
 			</div>
 		</div>
