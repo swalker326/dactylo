44c26e5faadd0e2c64cf283cf8fb9842b9fc7fa2
Shane <shane@swalker.dev>
2024-02-04 14:44:43 -0600

    moved action

--- apps/remix/app/routes/dashboard.admin/route.tsx
+++ apps/remix/app/routes/dashboard.admin/route.tsx
@@ -1,4 +1,29 @@
+import { prisma } from "@dactylo/db/index";
+import { invariant } from "@epic-web/invariant";
+import { ActionFunctionArgs } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+
+export async function action({ request }: ActionFunctionArgs) {
+	const formData = await request.formData();
+	console.log("FORM DATA: ");
+	for (const [key, value] of formData.entries()) {
+		console.log(key, value);
+	}
+	const { videoId, approve, remove } = Object.fromEntries(formData.entries());
+	invariant(videoId, "videoId is required");
+	if (videoId && remove === "REMOVED") {
+		await prisma.video.update({
+			where: { id: videoId as string },
+			data: { status: "REMOVED" },
+		});
+	} else if (videoId && approve === "APPROVED") {
+		await prisma.video.update({
+			where: { id: videoId as string },
+			data: { status: "ACTIVE" },
+		});
+	}
+	return null;
+}
 
 export default function DashboardAdminRoute() {
 	return <Outlet />;
