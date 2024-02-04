cbc3e42f6c5c28976b5f284712e525fb687daeb3
Shane <shane@swalker.dev>
2024-02-04 14:55:49 -0600

    format & removed query for prisma db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+	prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn', 'query'] : ['error'],
-  });
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+	});
 export type { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
