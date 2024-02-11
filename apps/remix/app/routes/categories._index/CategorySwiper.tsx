6b1b00b0772e5740d8f120ecf5a5bbf0b5d8c7c9
Shane <shane@swalker.dev>
2024-02-11 15:38:29 -0600

    WIP swiper dynamic height

--- apps/remix/app/routes/categories._index/CategorySwiper.tsx
+++ apps/remix/app/routes/categories._index/CategorySwiper.tsx
@@ -2,6 +2,7 @@ import { Link } from "@remix-run/react";
 import { UseDataFunctionReturn } from "remix-typedjson";
 import { VideoCard } from "~/components/video-card";
 import { loader } from "~/routes/categories._index/route";
+import "./style.css";
 
 export function CategorySwiper({
 	category,
@@ -12,38 +13,44 @@ export function CategorySwiper({
 }) {
 	return (
 		<div className="bg-white py-3 rounded-md">
-			<h2 className="px-1.5 text-2xl">{category.name}</h2>
+			<Link
+				to={`${category.slug}`}
+				className="px-1.5 text-3xl text-blue-700 dark:text-blue-500 font-bold"
+			>
+				{category.name}
+			</Link>
 			<swiper-container
 				style={{
 					"--swiper-navigation-color": "#2463EB",
 					"--swiper-pagination-color": "#000",
 				}}
 				slides-per-view="1"
-        autoHeight={true}
 				navigation={true}
 				pagination={true}
 			>
 				{category.signs.map((sign) => (
 					<swiper-slide key={sign.id}>
-						<h3 className="capitalize px-1.5">{sign.term.word}</h3>
 						{sign.videos.length === 0 ? (
-							<div className="flex items-center justify-center h-full w-full min-h-36">
-								<h4>
-									No videos for {sign.term.word} 😿{" "}
-									<Link
-										className="text-blue-500"
-										to={`/dashboard/create?signId=${sign.id}`}
-									>
-										create one!
-									</Link>
-								</h4>
-							</div>
+							<h4>
+								No videos for {sign.term.word} 😿{" "}
+								<Link
+									className="text-blue-500"
+									to={`/dashboard/create?signId=${sign.id}`}
+								>
+									create one!
+								</Link>
+							</h4>
+							// <div className="flex justify-center h-full border border-green-500">
+							// </div>
 						) : (
-							<VideoCard
-								key={sign.videos[0].id}
-								video={sign.videos[0]}
-								userId={userId || null}
-							/>
+							<>
+								<h3 className="capitalize px-1.5">{sign.term.word}</h3>
+								<VideoCard
+									key={sign.videos[0].id}
+									video={sign.videos[0]}
+									userId={userId || null}
+								/>
+							</>
 						)}
 					</swiper-slide>
 				))}
